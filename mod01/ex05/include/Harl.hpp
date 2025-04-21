6c7df2ae951cc8aa0ade23cf601189c2a5ba5394
eutrius <eutrius@gmail.com>
2025-04-22 00:44:18 +0200

    finish project structure

---
+++ mod01/ex05/include/Harl.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jyriarte <jyriarte@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/21 15:30:28 by jyriarte          #+#    #+#             */
+/*   Updated: 2025/04/21 15:36:09 by jyriarte         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+#define HARL_HPP
+
+#include <string>
+
+class Harl
+{
+  public:
+    Harl(void);
+    ~Harl(void);
+
+    void complain(std::string level);
+
+  private:
+    void debug(void);
+    void info(void);
+    void warning(void);
+    void error(void);
+};
+
+#endif
