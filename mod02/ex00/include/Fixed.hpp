1d9806304992b0bc8dd4b21fac306cf4ec5cbb77
John Vincent Yriarte <jyriarte@e3r6p1.42roma.it>
2025-05-02 14:48:04 +0200

    finish mod02 ex00-01

---
+++ mod02/ex00/include/Fixed.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jyriarte <jyriarte@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/29 16:53:08 by jyriarte          #+#    #+#             */
+/*   Updated: 2025/04/29 17:20:13 by jyriarte         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+class Fixed
+{
+  public:
+    Fixed(void);
+    Fixed(const Fixed &other);
+    ~Fixed(void);
+    Fixed &operator=(const Fixed &other);
+
+    int getRawBits(void) const;
+    void setRawBits(int const raw);
+
+  private:
+    static const int FRACTIONAL_BITS = 8;
+    int _value;
+};
+#endif
---
+++ mod02/ex00/src/Fixed.cpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jyriarte <jyriarte@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/29 17:20:21 by jyriarte          #+#    #+#             */
+/*   Updated: 2025/04/29 17:20:39 by jyriarte         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+#include <iostream>
+
+Fixed::Fixed(void) : _value(0)
+{
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &other)
+{
+    std::cout << "Copy constructor called" << std::endl;
+    *this = other;
+}
+
+Fixed::~Fixed(void)
+{
+    std::cout << "Destructor called" << std::endl;
+}
+
+Fixed &Fixed::operator=(const Fixed &other)
+{
+    std::cout << "Copy assignment operator called" << std::endl;
+    this->_value = other.getRawBits();
+    return (*this);
+}
+
+int Fixed::getRawBits(void) const
+{
+    std::cout << "getRawBits member function called" << std::endl;
+    return (this->_value);
+}
+
+void Fixed::setRawBits(int const raw)
+{
+    std::cout << "setRawBits member function called" << std::endl;
+    this->_value = raw;
+}
