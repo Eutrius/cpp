224d9b06910b1398b5e0159a9f56fde978ce4e6f
eutrius <eutrius@gmail.com>
2025-06-01 15:08:43 +0200

    fix headers, start tests, modify makefile

--- mod05/ex02/src/main.cpp
+++ mod05/ex02/src/main.cpp
@@ -6,204 +6,34 @@
 /*   By: jyriarte <jyriarte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/27 14:00:35 by jyriarte          #+#    #+#             */
-/*   Updated: 2025/05/27 14:19:32 by jyriarte         ###   ########.fr       */
+/*   Updated: 2025/06/01 13:56:00 by jyriarte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Bureaucrat.hpp"
+#include "PresidentialPardonForm.hpp"
+#include "RobotomyRequestForm.hpp"
 #include "ShrubberyCreationForm.hpp"
 #include "utils.h"
 
 int main(void)
 {
-    Bureaucrat *me;
-    std::cout << std::endl;
-    cPrint("*** Bureaucrat Test ***", 1, BLUE);
-    try
-    {
-        std::cout << std::endl;
-        cPrint(" * Test grade too high! * ", 1, GREEN);
-        me = new Bureaucrat("Vincent", -1);
-        std::cout << *me << std::endl;
-    }
-    catch (const std::exception &e)
-    {
-        std::cout << e.what() << std::endl;
-    }
-
-    try
-    {
-        std::cout << std::endl;
-        cPrint(" * Test grade too low! * ", 1, GREEN);
-        me = new Bureaucrat("Vincent", 151);
-        std::cout << *me << std::endl;
-    }
-    catch (const std::exception &e)
-    {
-        std::cout << e.what() << std::endl;
-    }
-
-    try
-    {
-        std::cout << std::endl;
-        cPrint(" * Test valid grade! * ", 1, GREEN);
-        me = new Bureaucrat("Vincent", 1);
-        std::cout << *me << std::endl;
-        delete me;
-    }
-    catch (const std::exception &e)
-    {
-        std::cout << e.what() << std::endl;
-    }
-
-    try
-    {
-        std::cout << std::endl;
-        cPrint(" * Test invalid increment! * ", 1, GREEN);
-        me = new Bureaucrat("Vincent", 1);
-        std::cout << *me << std::endl;
-        me->incrementGrade();
-        std::cout << *me << std::endl;
-    }
-    catch (const std::exception &e)
-    {
-        std::cout << e.what() << std::endl;
-        std::cout << *me << std::endl;
-        delete me;
-    }
+    cPrint("*** AForm Test ***", 1, BLUE);
 
     try
     {
         std::cout << std::endl;
         cPrint(" * Test invalid decrement! * ", 1, GREEN);
-        me = new Bureaucrat("Vincent", 150);
-        std::cout << *me << std::endl;
-        me->decrementGrade();
-        std::cout << *me << std::endl;
-    }
-    catch (const std::exception &e)
-    {
-        std::cout << e.what() << std::endl;
-        std::cout << *me << std::endl;
-        delete me;
-    }
-
-    try
-    {
-        std::cout << std::endl;
-        cPrint(" * Test everything valid * ", 1, GREEN);
-        cPrint("construction", 1, YELLOW);
-        me = new Bureaucrat("Vincent", 5);
-        std::cout << *me << std::endl;
-        cPrint("increment", 1, YELLOW);
-        me->incrementGrade();
-        std::cout << *me << std::endl;
-        cPrint("decrement", 1, YELLOW);
-        AForm *form = new ShrubberyCreationForm("hello");
-        me->signForm(*form);
+        Bureaucrat me("Me", 1);
+        std::cout << me << std::endl;
+        AForm *form = new RobotomyRequestForm("hello");
         std::cout << *form << std::endl;
-        me->executeForm(*form);
-        std::cout << *me << std::endl;
-        std::cout << std::endl;
-        delete me;
+        me.signForm(*form);
+        std::cout << *form << std::endl;
+        me.executeForm(*form);
     }
     catch (const std::exception &e)
     {
         std::cout << e.what() << std::endl;
     }
-
-    // std::cout << std::endl;
-    // cPrint("*** AForm Test ***", 1, BLUE);
-    // AForm *form;
-    //
-    // try
-    // {
-    //     std::cout << std::endl;
-    //     cPrint(" * Test gradeToSign too high! * ", 1, GREEN);
-    //     form = new AForm("AForm", 0, 150);
-    //     std::cout << *form << std::endl;
-    // }
-    // catch (const std::exception &e)
-    // {
-    //     std::cout << e.what() << std::endl;
-    // }
-    //
-    // try
-    // {
-    //     std::cout << std::endl;
-    //     cPrint(" * Test gradeToExecute too high! * ", 1, GREEN);
-    //     form = new AForm("AForm", 150, 0);
-    //     std::cout << *form << std::endl;
-    // }
-    // catch (const std::exception &e)
-    // {
-    //     std::cout << e.what() << std::endl;
-    // }
-    //
-    // try
-    // {
-    //     std::cout << std::endl;
-    //     cPrint(" * Test gradeToSign too low! * ", 1, GREEN);
-    //     form = new AForm("AForm", 151, 1);
-    //     std::cout << *form << std::endl;
-    // }
-    // catch (const std::exception &e)
-    // {
-    //     std::cout << e.what() << std::endl;
-    // }
-    //
-    // try
-    // {
-    //     std::cout << std::endl;
-    //     cPrint(" * Test gradeToExecute too low! * ", 1, GREEN);
-    //     form = new AForm("AForm", 1, 151);
-    //     std::cout << *form << std::endl;
-    // }
-    // catch (const std::exception &e)
-    // {
-    //     std::cout << e.what() << std::endl;
-    // }
-    //
-    // try
-    // {
-    //     std::cout << std::endl;
-    //     cPrint(" * Test valid form! * ", 1, GREEN);
-    //     form = new AForm("AForm", 1, 1);
-    //     std::cout << *form << std::endl;
-    //     delete form;
-    // }
-    // catch (const std::exception &e)
-    // {
-    //     std::cout << e.what() << std::endl;
-    // }
-    //
-    // std::cout << std::endl;
-    // cPrint(" * Test valid form! * ", 1, GREEN);
-    // me = new Bureaucrat("Vincent", 2);
-    // form = new AForm("AForm", 1, 1);
-    // std::cout << *me << std::endl;
-    // std::cout << *form << std::endl;
-    // std::cout << std::endl;
-    // cPrint("1st attempt", 1, YELLOW);
-    // me->signForm(form);
-    // try
-    // {
-    //     std::cout << std::endl;
-    //     cPrint("increment", 1, YELLOW);
-    //     me->incrementGrade();
-    //     std::cout << *me << std::endl;
-    // }
-    // catch (const std::exception &e)
-    // {
-    //     std::cout << e.what() << std::endl;
-    // }
-    // std::cout << std::endl;
-    // cPrint("2nd attempt", 1, YELLOW);
-    // me->signForm(form);
-    // std::cout << *form << std::endl;
-    //
-    // delete (me);
-    // delete (form);
-
     return (0);
 }
