bb9e0101a8dc646ae01d892f1bb3f5b4f1e36596
eutrius <eutrius@gmail.com>
2025-06-21 14:37:02 +0200

    clean 00, start 01

--- mod09/ex01/include/RPN.hpp
+++ mod09/ex01/include/RPN.hpp
@@ -1,28 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   utils.h                                            :+:      :+:    :+:   */
+/*   RPN.hpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jyriarte <jyriarte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/13 11:47:29 by jyriarte          #+#    #+#             */
-/*   Updated: 2025/05/13 12:10:35 by jyriarte         ###   ########.fr       */
+/*   Created: 2025/06/21 13:36:00 by jyriarte          #+#    #+#             */
+/*   Updated: 2025/06/21 13:36:07 by jyriarte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #pragma once
 
-#include <string>
-
-typedef enum e_color
+class RPN
 {
-	BLACK,
-	RED,
-	GREEN,
-	YELLOW,
-	BLUE,
-	PURPLE,
-	CYAN,
-	WHITE,
-}		t_color;
+   public:
+	RPN(void);
+	RPN(const RPN &other);
+	~RPN(void);
 
-void	cPrint(const std::string &str, int endl = 1, t_color color = WHITE);
+	RPN &operator=(const RPN &other);
+};
---
+++ mod09/ex01/src/main.cpp
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jyriarte <jyriarte@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/17 10:18:50 by jyriarte          #+#    #+#             */
+/*   Updated: 2025/06/18 18:12:50 by jyriarte         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "BitcoinExchange.hpp"
+
+int main(int argc, char **argv)
+{
+	std::string datafile = "data.csv";
+	std::string inputfile = "";
+
+	if (argc >= 2)
+		inputfile = argv[1];
+
+	BitcoinExchange *dex;
+	try
+	{
+		dex = new BitcoinExchange(datafile);
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+		return (1);
+	}
+
+	std::ifstream file(inputfile.c_str());
+	if (!file)
+	{
+		std::cerr << "Error: could not open file." << std::endl;
+		delete dex;
+		return (1);
+	}
+
+	std::string line;
+	std::getline(file, line);
+	if (line != "date | value")
+	{
+		std::cerr << "Error: input file corrupted." << std::endl;
+		delete dex;
+		return (1);
+	}
+
+	while (std::getline(file, line))
+		dex->exchange(line);
+
+	delete dex;
+
+	return (0);
+}
